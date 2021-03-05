5fd4ab5e88c9729f8658d535640ebedfbccbefa1
Aliaksandr <aliaksandr.bytsai@gmail.com>
2021-03-05 14:00:51 +0100

    feat: config mfe1 app

---
+++ apps/mfe1/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: 'home',  
+    loadChildren: './home/home.module#HomeModule',
+},
+  { path: '',   redirectTo: '/home', pathMatch: 'full' }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    [RouterModule.forRoot(routes)],
+    CommonModule
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
